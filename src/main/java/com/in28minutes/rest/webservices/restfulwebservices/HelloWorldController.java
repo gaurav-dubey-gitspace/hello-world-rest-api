d2573af2a672af42db6ea07f5dd5fde9cd04bd12
Gaurav <gaurav@Air.local>
2021-12-14 12:41:42 +0530

    today

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World - V6 - Green";
+		return "Hello World - V7 - Green";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
