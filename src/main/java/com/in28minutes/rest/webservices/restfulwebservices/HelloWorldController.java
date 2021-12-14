3e9f04a9be55dda963f5e827ec4e9a8f4bb42cee
Gaurav <gaurav@Air.local>
2021-12-14 13:04:50 +0530

    commit 71

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World - V81 - Green";
+		return "Hello World - V81 - Blue";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
