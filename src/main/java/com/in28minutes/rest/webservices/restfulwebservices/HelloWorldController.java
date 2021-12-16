2a4739ea4f7c20cd342c06fe0d8e780686308d9a
Gaurav <gaurav@Air.local>
2021-12-16 14:26:01 +0530

    sdf

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World - V54 - Green";
+		return "Hello World - V5489 - Green";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
